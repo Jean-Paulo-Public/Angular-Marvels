728e95bc8e742ac1c4c4d62c1d5da985a9b8777d
jean <jeanpauloriopreto.code@gmail.com>
2023-03-06 00:19:12 -0300

    Hello World

---
+++ Angular-Marvels/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- README.md
+++
@@ -1,5 +0,0 @@
-# Angular-Marvels
-
-Repositório em construção.
-
-Este repositório será usado como portfólio para demonstrar meus conhecimentos em programação web.
