2907282076911945cea726e631d061576a5996e4
jean <jeanpauloriopreto.code@gmail.com>
2023-03-08 21:46:40 -0300

    melhoria: adicionando ngFor

--- Angular-Marvels/src/app/app.component.ts
+++ Angular-Marvels/src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent {  
   isDarkTheme = false;
   pageTitle = 'Angular Marvels'
+  buttons = [
+    {value: 'personagens', text: 'Personagens'},
+    {value: 'historia', text: 'Histórias'},
+    {value: 'eventos', text: 'Eventos'},
+    {value: 'series', text: 'Séries'},
+    {value: 'comics', text: 'Comics'},
+    {value: 'criadores', text: 'Criadores'},
+    {value: 'favoritos', text: 'Favoritos'},
+    {value: 'curtidos', text: 'Curtidos'}
+  ];  
   onThemeChanged(theme: boolean) {
     this.isDarkTheme = theme; // muda a variável de acordo com o evento recebido 
   }
